8f072f3588955a050e63e6555e1b1cd587d16fcc
Jake Butler <JakeButler.student@careerdevs.com>
2022-10-03 22:45:14 -0400

    Wrote new method in AccountRepository

--- src/main/java/com/careerdevs/mybankingapp/repositories/AccountRepository.java
+++ src/main/java/com/careerdevs/mybankingapp/repositories/AccountRepository.java
@@ -2,10 +2,12 @@ package com.careerdevs.mybankingapp.repositories;
 
 
 import com.careerdevs.mybankingapp.models.CheckingModel;
+import com.careerdevs.mybankingapp.models.SavingsModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface AccountRepository extends JpaRepository<CheckingModel, Integer> {
     CheckingModel findOneByUuid(UUID uuid);
+//    SavingsModel findOneByUuid(UUID uuid);
 }
